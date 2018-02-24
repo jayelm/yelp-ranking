853fc19e0b7e96c570bb39b23118decfd2bb4a4c
Jesse Mu <jlm95@cam.ac.uk>
2018-02-24 01:09:00 +0000

    Fixup R plotting script (theoretically)

--- plot_matches_graph.R
+++ plot_matches_graph.R
@@ -2,13 +2,8 @@ library(tidyverse)
 library(igraph)
 library(data.table)
 
-ms_graph = fread('dataset_processed/matches.csv') %>%
-  tbl_df %>%
-  filter(win != 0) %>%
-  select(b1, b2) %>%
-  as.matrix
-ms_graph = ms_graph + 1
-ms_graph = graph_from_edgelist(ms_graph, directed = FALSE)
+ms_graph = read.dot('./dataset_processed/dedup_tinier.dot') %>%
+  graph.adjacency
 
 png('img/graph.png', 2000, 2000)
 plot(ms_graph)
