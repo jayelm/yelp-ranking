166ce28f7130a15cc3a5e2a8fbc0fa980db55052
Jesse Mu <jlm95@cam.ac.uk>
2018-02-25 03:09:53 +0000

    Add testing script

--- hiernorm.py
+++ hiernorm.py
@@ -16,6 +16,8 @@ import pandas as pd
 from preprocess_reviews import REVIEWS_FILE
 import pickle
 from hashlib import md5
+import numpy as np
+from scipy.stats import norm
 
 
 def StanModel_cache(model_code, model_name=None, **kwargs):
@@ -50,6 +52,8 @@ if __name__ == '__main__':
 
     parser.add_argument('--num_samples', default=100000, type=int,
                         help='Number of NUTS samples')
+    parser.add_argument('--test', action='store_true',
+                        help='Test with fake data')
 
     parser.add_argument(
         '--save',
@@ -79,15 +83,31 @@ if __name__ == '__main__':
     # Reindex reviews
     reviews.user = reviews.user.apply(lambda u: reindexed_users[u])
 
-    data = {
-        # Number of reviews
-        'n': reviews.shape[0],
-        # Number of users
-        'p': n_users,
-        # All reviews
-        'y': reviews.stars.values,
-        'user_idx': reviews.user.values
-    }
+    if args.test:
+        s1 = 100
+        s2 = 50
+        s3 = 10
+        u1 = norm(3, 1).rvs(size=s1)
+        u2 = norm(2, 0.2).rvs(size=s2)
+        u3 = norm(4, 0.5).rvs(size=s3)
+        data = {
+            'n': s1 + s2 + s3,
+            'p': 3,
+            'y': np.concatenate([u1, u2, u3]),
+            'user_idx': np.concatenate(
+                [np.repeat(1, s1), np.repeat(2, s2), np.repeat(3, s3)]
+            )
+        }
+    else:
+        data = {
+            # Number of reviews
+            'n': reviews.shape[0],
+            # Number of users
+            'p': n_users,
+            # All reviews
+            'y': reviews.stars.values,
+            'user_idx': reviews.user.values
+        }
 
     with open(MODEL, 'r') as fin:
         model_code = fin.read()
